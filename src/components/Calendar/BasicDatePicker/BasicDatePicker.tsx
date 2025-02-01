c452c087a4d544a92f266b4aa831da9636f629fa
gamitej <gamitejpratapsingh998@gmail.com>
2025-02-01 23:42:12 +0530

    NEW : basic date picker done

--- src/components/Calendar/BasicDatePicker/BasicDatePicker.tsx
+++ src/components/Calendar/BasicDatePicker/BasicDatePicker.tsx
@@ -1,8 +1,9 @@
-import { monthsOptions, weeks, yearsOptions } from "./data";
+import { useMemo, useState } from "react";
 // icons
 import { IoCalendar as CalendarIcon } from "react-icons/io5";
 import { IoIosArrowBack as ArrowIcon } from "react-icons/io";
-import { useMemo, useState } from "react";
+// data
+import { getTheDay, getTodaysDay, monthsOptions, weeks } from "./data";
 
 interface BasicDatePickerProps {
   date?: string | null;
@@ -13,7 +14,6 @@ const BasicDatePicker = ({ date }: BasicDatePickerProps) => {
 
   const toggleButton = () => setIsOpen((state) => !state);
 
-  //   console.log({ yearsOptions });
   /**
    * TSX
    */
@@ -21,7 +21,7 @@ const BasicDatePicker = ({ date }: BasicDatePickerProps) => {
     <div className="relative">
       <button
         onClick={toggleButton}
-        className="rounded-sm cursor-pointer select-none bg-gray-100 px-4 py-2 w-[20rem] flex justify-between items-center focus:ring-2 focus:ring-blue-200"
+        className="rounded-sm cursor-pointer select-none bg-gray-100 px-4 py-2 w-[15rem] flex justify-between items-center focus:ring-2 focus:ring-blue-200"
       >
         <span className="text-lg text-gray-700">{date ?? "dd/mm/yyyy "}</span>
         <span>
@@ -31,80 +31,97 @@ const BasicDatePicker = ({ date }: BasicDatePickerProps) => {
 
       <div
         aria-hidden={!isOpen}
-        className="aria-hidden:hidden absolute w-full z-100 mt-2 bg-gray-50 rounded-sm shadow border select-none"
+        className="aria-hidden:hidden absolute w-[20rem] z-100 mt-2 bg-gray-50 rounded-sm shadow border select-none"
       >
-        <DateCard />
+        <WeekAndDaysCardLayout />
       </div>
     </div>
   );
 };
 
-function DateCard() {
-  const [currentDate, setCurrentDate] = useState(() => {
-    const date = new Date().getDate();
-    const month = new Date().getMonth();
-    const year = new Date().getFullYear();
+function WeekAndDaysCardLayout() {
+  const [currentDate, setCurrentDate] = useState(() => getTodaysDay());
 
-    return { date, month, year };
-  });
-
-  const date = useMemo(() => {
-    return monthsOptions.find((item) => item.month === currentDate.month);
-  }, [currentDate]);
-
-  const handleMonthNext = () => {
-    if (currentDate.month === 12) {
+  const handleNextMonth = () => {
+    if (currentDate.month === 12)
       setCurrentDate((state) => ({ ...state, month: 1, year: state.year + 1 }));
-    } else {
-      setCurrentDate((state) => ({ ...state, month: state.month + 1 }));
-    }
+    else setCurrentDate((state) => ({ ...state, month: state.month + 1 }));
   };
 
-  const handleMonthPrev = () => {
+  const handlePrevMonth = () => {
     if (currentDate.month === 1) {
       setCurrentDate((state) => ({
         ...state,
         month: 12,
         year: state.year - 1,
       }));
-    } else {
-      setCurrentDate((state) => ({ ...state, month: state.month - 1 }));
-    }
+    } else setCurrentDate((state) => ({ ...state, month: state.month - 1 }));
   };
+
+  const date = useMemo(() => {
+    const dateInfo = monthsOptions.find(
+      (item) => item.month === currentDate.month
+    ) || { days: 0, shortName: "" };
+
+    // get no of day in a month and starting week day
+    const weekDay = getTheDay(currentDate) || { no: 1 };
+    const emptyFillArray = Array(weekDay?.no - 1).fill(" ");
+    for (let i = 0; i < dateInfo?.days; i++) emptyFillArray.push(i + 1);
+
+    // const dateFormat = `${currentDate.year}/${currentDate.month}/${currentDate.date}`;
+
+    return { ...dateInfo, day: weekDay, daysArray: emptyFillArray };
+  }, [currentDate]);
 
   /**
    * TSX
    */
   return (
     <div>
+      {/* ======================= SELECT OPTIONS ======================== */}
       <div className="w-full grid grid-cols-12 text-gray-700 overflow-hidden border-b">
         <span
-          onClick={handleMonthPrev}
+          onClick={handlePrevMonth}
           className="rounded-sm col-span-3 flex justify-center items-center hover:bg-gray-200 cursor-pointer p-1 focus:ring-2 focus:ring-blue-200"
         >
           <ArrowIcon className="text-2xl" />
         </span>
         <span className="border-x rounded-sm col-span-6 text-center text-lg hover:bg-gray-200 cursor-pointer p-1">
-          {date?.shortLabel} - {currentDate.year}
+          {date?.shortName} {currentDate.year}
         </span>
         <span
-          onClick={handleMonthNext}
+          onClick={handleNextMonth}
           className="rounded-sm col-span-3 flex justify-center items-center hover:bg-gray-200 cursor-pointer p-1 focus:ring-2 focus:ring-blue-200"
         >
           <ArrowIcon className="-rotate-180 text-2xl" />
         </span>
       </div>
-      <div className="h-[10rem]">
+
+      <div className="h-fit">
+        {/* ======================= WEEKS ======================== */}
         <div className="grid grid-cols-7 border-b">
-          {weeks.map(({ label }, idx) => (
+          {weeks.map(({ week }, idx) => (
             <span
-              title={label}
-              key={`${label}-${idx}`}
+              title={week}
+              key={`${week}-${idx}`}
               className={`col-span-1 ${
                 idx % 2 === 0 ? "bg-gray-100" : ""
-              } text-center`}
+              } text-center text-gray-600`}
             >
-              {label.slice(0, 1)}
+              {week.slice(0, 1)}
+            </span>
+          ))}
+        </div>
+        {/* ======================= DAYS ======================== */}
+        <div className="grid grid-cols-7 border-b">
+          {date.daysArray.map((val, idx) => (
+            <span
+              key={`${val}-${idx}`}
+              className={`col-span-1 ${
+                idx % 2 === 0 ? "bg-gray-100" : ""
+              } text-center text-gray-600 hover:bg-blue-200`}
+            >
+              {val}
             </span>
           ))}
         </div>
--- src/components/Calendar/BasicDatePicker/data.ts
+++ src/components/Calendar/BasicDatePicker/data.ts
@@ -1,30 +1,35 @@
 export const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  { month: "January", days: 31 },
+  { month: "February", days: 28 }, // 29 in leap years
+  { month: "March", days: 31 },
+  { month: "April", days: 30 },
+  { month: "May", days: 31 },
+  { month: "June", days: 30 },
+  { month: "July", days: 31 },
+  { month: "August", days: 31 },
+  { month: "September", days: 30 },
+  { month: "October", days: 31 },
+  { month: "November", days: 30 },
+  { month: "December", days: 31 },
 ] as const;
 
 export const weeks = [
-  { no: 1, label: "Sunday" },
-  { no: 2, label: "Monday" },
-  { no: 3, label: "Tuseday" },
-  { no: 4, label: "Wednesday" },
-  { no: 5, label: "Thursday" },
-  { no: 6, label: "Friday" },
-  { no: 7, label: "Saturday" },
+  { no: 1, week: "Sunday" },
+  { no: 2, week: "Monday" },
+  { no: 3, week: "Tuseday" },
+  { no: 4, week: "Wednesday" },
+  { no: 5, week: "Thursday" },
+  { no: 6, week: "Friday" },
+  { no: 7, week: "Saturday" },
 ] as const;
 
-export const monthsOptions = months.map((month: string, idx: number) => {
-  return { month: idx + 1, shortLabel: month.slice(0, 3), fullLabel: month };
+export const monthsOptions = months.map(({ month, days }, idx: number) => {
+  return {
+    days,
+    month: idx + 1,
+    fullName: month,
+    shortName: month.slice(0, 3),
+  };
 });
 
 export const yearsOptions = Array.from({ length: 2099 - 1900 + 1 }).map(
@@ -32,3 +37,32 @@ export const yearsOptions = Array.from({ length: 2099 - 1900 + 1 }).map(
     return 1900 + idx;
   }
 );
+
+export const checkLeapYear = (year: number): boolean => {
+  const days =
+    year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? 29 : 28;
+
+  return days === 29;
+};
+
+/**
+ * ==================== GET THE DAY ======================
+ */
+
+export const getTheDay = <T extends { year: number; month: number }>(
+  currentDate: T
+): { no: number; week: string } | undefined => {
+  const dateFormat = `${currentDate.year}/${currentDate.month}/1`;
+  const day = new Date(dateFormat).getDay();
+  const weekDay = weeks.find((w) => w.no === day + 1);
+
+  return weekDay;
+};
+
+export const getTodaysDay = () => {
+  const date = new Date().getDate();
+  const month = new Date().getMonth() + 1;
+  const year = new Date().getFullYear();
+
+  return { date, month, year };
+};
