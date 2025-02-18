2011112bda1ab6a22278d593898d72ba852d4260
gamitej <gamitejpratapsingh998@gmail.com>
2025-02-18 19:22:50 +0530

    chore : api call done for search input

--- src/components/Dropdowns/MultiSelect/Select.tsx
+++ src/components/Dropdowns/MultiSelect/Select.tsx
@@ -1,35 +1,44 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Select = () => {
-  const [searchInput, setSeachInput] = useState<string>("");
   const [suggestion, setSuggestion] = useState([]);
+  const [searchInput, setSeachInput] = useState<string>("");
 
   useEffect(() => {
     const fetchSugg = async () => {
-      const url = `http://dummyjson.com/users/search?q=${searchInput}`;
-
-      const res = await fetch(url);
+      const res = await fetch(
+        `http://dummyjson.com/users/search?q=${searchInput}`
+      );
       const data = await res.json();
-
-      console.log(data);
+      const nameList = data?.users?.map(
+        ({ firstName, lastName }: { firstName: string; lastName: string }) =>
+          `${firstName} ${lastName}`
+      );
+      setSuggestion(nameList);
     };
-
-    fetchSugg();
+    if (searchInput.trim() !== "") fetchSugg();
   }, [searchInput]);
 
   /**
    * TSX
    */
   return (
-    <div className="w-full flex flex-col justify-center items-center">
+    <div className="relative w-full flex flex-col justify-center items-center">
       <input
         type="text"
+        spellCheck="false"
         value={searchInput}
         placeholder="enter search..."
-        className="w-full px-4 py-3 rounded-md focus:outline-0 focus:ring-2 focus:ring-blue-300 text-2xl"
         onChange={(e) => setSeachInput(e.target.value)}
+        className="w-full px-4 py-3 rounded-md focus:outline-0 focus:ring-2 focus:ring-blue-300 text-2xl shadow-sm"
       />
-      <div></div>
+      <div className="absolute w-full top-[4rem] max-h-[15rem] overflow-auto flex flex-col justify-center items-center bg-white rounded-md shadow-sm">
+        {suggestion.map((name) => (
+          <p key={name} className="py-2">
+            {name}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
