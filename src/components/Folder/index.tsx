018b571969fead1af79e50e4bff341a2415c54e9
gamitej <gamitejpratapsingh998@gmail.com>
2025-02-19 22:11:00 +0530

    chore : folder structure show hide done

--- src/components/Folder/index.tsx
+++ src/components/Folder/index.tsx
@@ -1,46 +1,62 @@
+import { useState } from "react";
 import { FaFolderOpen as FolderIcon } from "react-icons/fa";
 import { FaRegFileLines as FileIcon } from "react-icons/fa6";
 import { data } from "./data";
 
-const Folder = () => {
-  const Recursive = ({ data = [], marginLeft = 0 }) => {
-    if (data.length === 0) return null;
+const Recursive = ({ data = [], marginLeft = 0, toggle, isFolderOpen }) => {
+  if (!data.length) return null;
 
-    return (
-      <div className="flex flex-col gap-2">
-        {data.map(({ id, isFolder, label, items = [] }) => {
-          const Icon = isFolder ? (
-            <FolderIcon className="text-gray-600 text-xl" />
-          ) : (
-            <FileIcon className="text-gray-500 text-xl" />
-          );
+  return (
+    <div className="flex flex-col gap-2">
+      {data.map(({ id, isFolder, label, items = [] }) => {
+        const isOpen = isFolderOpen[id] || false;
+        const Icon = isFolder ? (
+          <FolderIcon className="text-gray-600 text-xl" />
+        ) : (
+          <FileIcon className="text-gray-500 text-xl" />
+        );
 
-          return (
+        return (
+          <div key={id} style={{ marginLeft: `${marginLeft}rem` }}>
             <div
-              key={id}
-              style={{ marginLeft: `${marginLeft}rem` }}
-              className="flex flex-col gap-2"
+              onClick={() => isFolder && toggle(id)}
+              className="flex gap-2 cursor-pointer hover:underline"
             >
-              <div className="flex gap-2 cursor-pointer hover:underline">
-                <span>{Icon}</span>
-                <span>{label}</span>
-              </div>
-              {isFolder && items?.length > 0 && (
-                <Recursive data={items} marginLeft={marginLeft + 2} />
-              )}
+              <span>{Icon}</span>
+              <span>{label}</span>
             </div>
-          );
-        })}
-      </div>
-    );
+            {isOpen && isFolder && items.length > 0 && (
+              <div className="flex flex-col gap-2 mt-2">
+                <Recursive
+                  data={items}
+                  toggle={toggle}
+                  isFolderOpen={isFolderOpen}
+                  marginLeft={marginLeft + 1.5}
+                />
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const Folder = () => {
+  const [isFolderOpen, setIsFolderOpen] = useState<Record<number, boolean>>({});
+
+  const toggle = (id: number) => {
+    setIsFolderOpen((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
-  /**
-   * TSX
-   */
   return (
     <div className="p-8">
-      <Recursive data={data ?? []} marginLeft={0} />
+      <Recursive
+        data={data}
+        marginLeft={0}
+        toggle={toggle}
+        isFolderOpen={isFolderOpen}
+      />
     </div>
   );
 };
